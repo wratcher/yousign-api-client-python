a462c32a76f5696d2a5cce50fe031225201ddb77
Antoine Pinsard <antoine.pinsard@gmail.com>
2017-04-20 23:18:10 +0200

    Use relative imports

--- ysApi/__init__.py
+++ ysApi/__init__.py
@@ -1,5 +1,4 @@
-from apiClient import ApiClient
-from fileToSign import FileToSign, Signer, VisibleOptions
+from .apiClient import ApiClient
+from .fileToSign import FileToSign, Signer, VisibleOptions
+
 __all__ = ['ApiClient', 'FileToSign', 'Signer', 'VisibleOptions']
-
-
